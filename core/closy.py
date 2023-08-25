3eb4fcd365220da4e58a6be3e4365e298714d371
yi1z <yilz.zhang@mail.utoronto.ca>
2023-08-25 15:10:57 +0800

    something

--- core/closy.py
+++ core/closy.py
@@ -1,17 +1,32 @@
 from __future__ import annotations
 
+from recorder import Recorder
+
 
 # The virtual assistant
 class Closy:
     
     def __init__(self):
-        pass
+        self.is_running = False
+        self.is_listening = True
+        self.is_speaking = False
+
+        self.recorder = Recorder()
+        
+        # the current model state of the virtual assistant
+        self.curr_emotion = None
+        self.curr_movement = None
+        # the history of the model state of the virtual assistant
+        # this will be used as referenced supplied to GPT
+        self.emotion_history = {}
+        self.movement_history = {}
+        self.responses = {}
 
     def wake(self):
-        pass
+        self.is_running = True
 
     def rest(self):
-        pass
+        self.is_running = False
 
     def listen(self):
         pass

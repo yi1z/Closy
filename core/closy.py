fa1392701021181a62332d099452608b772fe216
yi1z <yilz.zhang@mail.utoronto.ca>
2023-08-25 16:58:27 +0800

    initially implemented emotion module

--- core/closy.py
+++ core/closy.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 
 from recorder import Recorder
+import emotion
+from configs import closy_config_handler as cch
 
 
 # The virtual assistant
 class Closy:
     
     def __init__(self):
+        # load the config
+        cch.load_config()
+
         self.is_running = False
         self.is_listening = True
         self.is_speaking = False
@@ -14,13 +19,32 @@ class Closy:
         self.recorder = Recorder()
         
         # the current model state of the virtual assistant
-        self.curr_emotion = None
-        self.curr_movement = None
+        self.curr_emotion = cch.closy_wake_emotion
+        self.curr_movement = cch.closy_wake_movement
+
         # the history of the model state of the virtual assistant
         # this will be used as referenced supplied to GPT
         self.emotion_history = {}
         self.movement_history = {}
         self.responses = {}
+
+        self.model = cch.closy_model
+        self.voice = cch.closy_voice
+        self.wake_emotion = cch.closy_wake_emotion
+        self.max_emotion = cch.closy_max_emotion
+        self.min_emotion = cch.closy_min_emotion
+        # how drastic it is for Closy to change her emotion due to an influence
+        self.emo_factor = cch.closy_emo_factor
+        # all movements Closy can take
+        self.movement_lists = cch.closy_movement_lists
+        # initial movement
+        self.wake_movement = cch.closy_wake_movement
+        # map from emotion to movement
+        self.emo_to_mov = cch.closy_emo_to_mov
+        # how often Closy would talk when she wakes up
+        self.wake_talk_rate = cch.closy_wake_talk_rate
+        # how often Closy would talk when she is in a certain emotion
+        self.emo_to_talk_rate = cch.closy_emo_to_talk_rate
 
     def wake(self):
         self.is_running = True
@@ -34,8 +58,20 @@ class Closy:
     def make_response(self):
         pass
 
-    def change_emotion(self):
-        pass
+    def change_emotion(self, text):
+        # analysis the emotion of the text
+        emo_score = emotion.get_emo(text)
+        # if the request returns -1, do nothing
+        if emo_score == -1:
+            return
+        # if the request returns a valid score, update Closy's emotion
+        self.curr_emotion += emo_score * self.emo_factor
+        # if the emotion is out of bound, set it to the bound
+        if self.curr_emotion > self.max_emotion:
+            self.curr_emotion = self.max_emotion
+        elif self.curr_emotion < self.min_emotion:
+            self.curr_emotion = self.min_emotion
+
 
     def make_movement(self):
         pass
---
+++ core/emotion.py
@@ -0,0 +1,46 @@
+import time
+import urllib.request
+import urllib.parse
+import json
+import hashlib
+import base64
+
+
+app_id="9d39e9ed"
+api_key="0c8ac079b6dffec57125c48eb83dcfcf"
+url ="http://ltpapi.xfyun.cn/v2/sa"
+
+
+# send text to emotion analysis api
+def analysis_emo(text):
+    body = urllib.parse.urlencode({'text': text}).encode('utf-8')
+    param = {"type": "dependent"}
+    x_param = base64.b64encode(json.dumps(param).replace(' ', '').encode('utf-8'))
+    x_time = str(int(time.time()))
+    x_checksum = hashlib.md5(api_key.encode('utf-8') + str(x_time).encode('utf-8') + x_param).hexdigest()
+    x_header = {'X-Appid': app_id,
+                'X-CurTime': x_time,
+                'X-Param': x_param,
+                'X-CheckSum': x_checksum}
+    req = urllib.request.Request(url, body, x_header)
+    result = urllib.request.urlopen(req)
+    result = result.read()
+    print(result.decode('utf-8'))
+    return
+
+
+# get the emotion score
+def get_emo(text):
+    result = analysis_emo(text)
+    # if the request is not success, return -1
+    if result['desc'] != 'success':
+        return -1
+    return float(result['data']['score'])
+
+
+# get sentiment
+def get_sentiment(text):
+    result = analysis_emo(text)
+    if result['desc'] == 'success':
+        return int(result['data']['sentiment']) + 1
+    return -1
